fe9e6cc8d4adb815dd271a3a0c2e820c39bc83db
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-07 10:18:55 -0700

    add punctuation to make output easier to read.

--- lib/combo.rb
+++ lib/combo.rb
@@ -31,16 +31,16 @@ class Change
       end
       return_string = String.new()
       if @coins["quarters"] != 0
-        return_string.concat("Quarters : #{@coins['quarters']} ,")
+        return_string.concat("Quarters : #{@coins['quarters']}, ")
       end
       if @coins["dimes"] != 0
-        return_string.concat("Dimes : #{@coins['dimes']} ,")
+        return_string.concat("Dimes : #{@coins['dimes']}, ")
       end
       if @coins["nickels"] != 0
-        return_string.concat("Nickels : #{@coins['nickels']} ,")
+        return_string.concat("Nickels : #{@coins['nickels']}, ")
       end
       if @coins["pennies"] != 0
-        return_string.concat("Pennies : #{@coins['pennies']} .")
+        return_string.concat("Pennies : #{@coins['pennies']}. ")
       end
       return_string
     end
--- spec/combo_spec.rb
+++ spec/combo_spec.rb
@@ -5,19 +5,19 @@ require('combo.rb')
 describe ('#combo') do
   it('will output value of quarters') do
   change = Change.new(1.00)
-  expect(change.coin_combos()).to(eq("Quarters : 4 "))
+  expect(change.coin_combos()).to(eq("Quarters : 4, "))
   end
 
   it('will output value of dimes') do
    dimes = Change.new(0.20)
-   expect(dimes.coin_combos()).to(eq("Dimes : 2 "))
+   expect(dimes.coin_combos()).to(eq("Dimes : 2, "))
   end
   it('will output value of nickels') do
     nickels = Change.new(0.05)
-    expect(nickels.coin_combos()).to(eq("Nickels : 1 "))
+    expect(nickels.coin_combos()).to(eq("Nickels : 1, "))
   end
   it('will output value for all coins in a random amount of change') do
     change = Change.new(0.94)
-    expect(change.coin_combos()).to(eq("Quarters : 3 Dimes : 1 Nickels : 1 Pennies : 4 "))
+    expect(change.coin_combos()).to(eq("Quarters : 3, Dimes : 1, Nickels : 1, Pennies : 4. "))
   end
 end
