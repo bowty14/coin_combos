75ca53dcbf1f83798ff6e5943bed57bb73822322
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-07 09:40:01 -0700

    refactored code so that all tests are now passing.

--- lib/combo.rb
+++ lib/combo.rb
@@ -1,13 +1,38 @@
 class Change
-  attr_accessor(:value)
-
+  attr_accessor(:coins)
+  attr_reader(:value)
   def initialize(value)
     @value = value
-    @coin_words = []
+    @coins = { 
+    "quarters" => 0, 
+    "dimes" => 0, 
+    "nickels" => 0,
+    "pennies" => 0
+    }
   end
 
-  def coin_combos
-    @coin_words = [0, "quarters",0,"dimes",0,"nickels",0,"pennies"]
-    cents = value * 100
+    def coin_combos
+      cents = @value * 100
+      
+      while (cents > 0)
+        if (cents / 25) >= 1
+          cents -= 25
+          @coins["quarters"] += 1
+        elsif (cents / 10) >= 1
+          cents -= 10
+          @coins["dimes"] +=1
+        elsif (cents / 5) >= 1
+          cents -= 5
+          @coins["nickels"] += 1
+        else
+          cents -= 1
+          @coins["pennies"] += 1
+        end
+      end
+      return_string = String.new()
+      if @coins["quarters"] != 0
+        return_string.concat("Quarters : #{@coins['quarters']} ")
+      end
+      return_string
+    end
   end
-end
--- spec/combo_spec.rb
+++ spec/combo_spec.rb
@@ -5,11 +5,11 @@ require('combo.rb')
 describe ('#combo') do
   it('will find a coin value when the inputted number is 1, 5, 10, or 25') do
   change = Change.new(1.00)
-  expect(change.coin_combos()).to(eq(100))
+  expect(change.coin_combos()).to(eq("Quarters : 4 "))
   end
 
   it('will find a coin calue when the inputted number is 1,5,10,or 25') do
-    quarter = Change.new(0.25)
-    expect(quarter.conin_combos()).to(eq(['quarter']))
+   quarter = Change.new(0.75)
+   expect(quarter.coin_combos()).to(eq("Quarters : 3 "))
   end
 end
