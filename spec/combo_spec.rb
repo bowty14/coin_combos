71312cdd0efcb925a1e545f2f9a56968ff064ea3
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-05-06 20:19:47 -0700

    add test for second spec, expect to fail.

--- spec/combo_spec.rb
+++ spec/combo_spec.rb
@@ -7,4 +7,9 @@ describe ('#combo') do
   change = Change.new(1.00)
   expect(change.coin_combos()).to(eq(100))
   end
+
+  it('will find a coin calue when the inputted number is 1,5,10,or 25') do
+    quarter = Change.new(0.25)
+    expect(quarter.conin_combos()).to(eq(['quarter']))
+  end
 end
